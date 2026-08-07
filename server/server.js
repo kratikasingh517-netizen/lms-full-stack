34d537fd26fb3cdc3395c644542604dd99146bf3
kratikasingh517-netizen <kratikasingh517@gmail.com>
2026-08-08 01:03:04 +0530

    Refactor server.js for improved connection and CORS
    Refactor server.js to connect to database and Cloudinary without await. Update CORS settings to specify allowed origin and credentials. Adjust route handlers for consistency and export app for Vercel serverless deployment.

--- server/server.js
+++ server/server.js
@@ -12,25 +12,40 @@ import courseRouter from './routes/courseRoute.js'
 // Initialize Express
 const app = express()
 
-// Connect to database
-await connectDB()
-await connectCloudinary()
+// Connect Database and Cloudinary
+connectDB()
+connectCloudinary()
 
 // Middlewares
-app.use(cors())
+app.use(cors({
+    origin: [
+        "https://lms-full-stack-beige.vercel.app"
+    ],
+    credentials: true
+}))
+
 app.use(clerkMiddleware())
 
 // Routes
-app.get('/', (req, res) => res.send("API Working"))
-app.post('/clerk', express.json() , clerkWebhooks)
-app.post('/stripe', express.raw({ type: 'application/json' }), stripeWebhooks)
+app.get('/', (req, res) => {
+    res.send("API Working")
+})
+
+app.post('/clerk', express.json(), clerkWebhooks)
+
+app.post(
+    '/stripe',
+    express.raw({ type: 'application/json' }),
+    stripeWebhooks
+)
+
 app.use('/api/educator', express.json(), educatorRouter)
+
 app.use('/api/course', express.json(), courseRouter)
+
 app.use('/api/user', express.json(), userRouter)
 
-// Port
-const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-})
+// Export for Vercel Serverless
+export default app
+export default app
